9c02f7a247b543b3c9b2cbc70050dde660d0f3aa
Luis Ramirez <146735904+LuisRamirezv@users.noreply.github.com>
2025-04-14 13:36:43 +0100

    Adding the button controls from extras.js to the script.js file

--- script.js
+++ script.js
@@ -226,3 +226,15 @@ addFolderButton.addEventListener("click", () => {
     addSongsFromFolder();
 });
 
+// Window control buttons
+document.getElementById('buttonred').addEventListener('click', () => {
+    window.musicAPI.controlWindow('close');
+  });
+  
+  document.getElementById('buttonyellow').addEventListener('click', () => {
+    window.musicAPI.controlWindow('minimize');
+  });
+  
+  document.getElementById('buttongreen').addEventListener('click', () => {
+    window.musicAPI.controlWindow('maximize');
+  });
